d1c3c3d4caccf9b51f51d26c51e5291f7a4ffe24
Keita Urashima <ursm@ursm.jp>
2021-11-20 03:35:41 +0900

    Merge specs

--- spec/example_spec.rb
+++ spec/example_spec.rb
@@ -7,16 +7,20 @@ RSpec.describe 'let_as' do
     2.let_as :two
   end
 
-  let(:three) { 3.let_as(:three_2) }
+  let!(:_three) { 3.let_as(:three) }
+
+  def let_four
+    4.let_as :four
+  end
 
   example do
-    four_2 = 4.let_as(:four)
+    let_four
+    5.let_as :five
 
-    expect(one).to     eq(1)
-    expect(two).to     eq(2)
-    expect(three).to   eq(3)
-    expect(three_2).to eq(3)
-    expect(four).to    eq(4)
-    expect(four_2).to  eq(4)
+    expect(one).to   eq(1)
+    expect(two).to   eq(2)
+    expect(three).to eq(3)
+    expect(four).to  eq(4)
+    expect(five).to  eq(5)
   end
 end
